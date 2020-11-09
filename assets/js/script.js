c6e74a14788f338652708428a14f844ecc1ac3d1
Paul Charpie <paul.charpie@snhu.edu>
2020-11-08 22:12:20 -0500

    added functionality to progress and volume bars

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,49 @@
 var currentPlaylist = [];
 var audioElement;
+var mouseDown = false;
 
+function formatTime(seconds) {
+  var time = Math.round(seconds);
+  var minutes = Math.floor(time / 60);
+  var seconds = time - minutes * 60;
+
+  var extraZero = (seconds < 10) ? "0" : "";
+  return minutes + ":" + extraZero + seconds;
+}
+
+function updateTimeProgressBar(audio) {
+  $(".progressTime.current").text(formatTime(audio.currentTime));
+  $(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+
+  var progress = audio.currentTime / audio.duration * 100;
+  $(".playbackBar .progress").css("width", progress + "%");
+}
+
+function updateVolumeProgressBar(audio){
+  var volume = audio.volume * 100;
+  $(".volumeBar .progress").css("width", volume + "%");
+}
 
 function Audio(){
 
   this.currentlyPlaying;
   this.audio = document.createElement('audio');
+
+  this.audio.addEventListener("canplay", function(){
+    var duration = formatTime(this.duration);
+    $(".progressTime.remaining").text(duration);
+    updateVolumeProgressBar(this);
+  });
+
+  this.audio.addEventListener("timeupdate", function(){
+    if(this.duration) {
+      updateTimeProgressBar(this);
+    }
+  });
+
+  this.audio.addEventListener("volumechange", function(){
+    updateVolumeProgressBar(this);
+  });
 
   this.setTrack = function(track) {
     this.currentlyPlaying = track;
@@ -19,4 +57,9 @@ function Audio(){
   this.pause = function(){
     this.audio.pause();
   }
+
+  this.setTime = function(seconds){
+    this.audio.currentTime = seconds;
+  }
+
 }
--- includes/nowPlayingBarContainer.php
+++ includes/nowPlayingBarContainer.php
@@ -16,8 +16,56 @@ $(document).ready(function(){
   currentPlaylist = <?php echo $jsonArray; ?>;
   audioElement = new Audio();
   setTrack(currentPlaylist[0], currentPlaylist, false);
+  updateVolumeProgressBar(audioElement.audio);
+
+//control progressbar slider
+  $(".playbackBar .progressBar").mousedown(function(){
+    mouseDown = true;
+  });
+  $(".playbackBar .progressBar").mousemove(function(e){
+    if(mouseDown) {
+      //Set time of song, depending on position of mouseDown
+      timeFromOffset(e, this);
+    }
+  });
+  $(".playbackBar .progressBar").mouseup(function(e){
+      timeFromOffset(e, this);
+  });
+
+  $(document).mouseup(function(){
+    mouseDown = false;
+  });
+
+  //control volumebar slider
+    $(".volumeBar .progressBar").mousedown(function(){
+      mouseDown = true;
+    });
+    $(".volumeBar .progressBar").mousemove(function(e){
+      if(mouseDown) {
+        var percentage = e.offsetX / $(this).width();
+        if(percentage>=0 && percentage<=1){
+          audioElement.audio.volume = percentage;
+        }
+      }
+    });
+    $(".volumeBar .progressBar").mouseup(function(e){
+      var percentage = e.offsetX / $(this).width();
+      if(percentage>=0 && percentage<=1){
+        audioElement.audio.volume = percentage;
+      }
+    });
+
+    $(document).mouseup(function(){
+      mouseDown = false;
+    });
 
 });
+
+function timeFromOffset(mouse, progressBar){
+  var percentage = mouse.offsetX / $(progressBar).width() * 100;
+  var seconds = audioElement.audio.duration * (percentage / 100);
+  audioElement.setTime(seconds);
+};
 
 function setTrack(trackId, newPlaylist, play){
 
