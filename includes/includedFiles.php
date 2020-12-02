58f2fac61ac614aeea563ec8a783eb78bdba1b81
Paul Charpie <paul@charpies.com>
2020-12-01 22:03:22 -0500

    created User & Playlist classes

--- includes/includedFiles.php
+++ includes/includedFiles.php
@@ -3,9 +3,19 @@
 //http_x_requested_with is included in ajax calls, so this finds ajax requested pages
 if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
     include("includes/config.php");
+    include("includes/classes/User.php");
     include("includes/classes/Artist.php");
     include("includes/classes/Album.php");  //Artist.php must come first because Album references it
     include("includes/classes/Song.php");
+    include("includes/classes/Playlist.php");
+
+    if(isset($_GET['userLoggedIn'])){
+        $userLoggedIn = new User($con, $_GET['userLoggedIn']);
+    }
+    else {
+        echo "username not passed into page. Check openPage().";
+        exit;
+    }
 }
 else {
     include("includes/header.php");
--- yourMusic.php
+++ yourMusic.php
@@ -11,6 +11,34 @@ include("includes/includedFiles.php");
         <div class="buttonItems">
             <button class="button blue" onclick="createPlaylist('something')">New Playlist</button>
         </div>
+
+    <?php
+    $username = $userLoggedIn->getUsername();
+
+      $playlistsQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner='$username'");
+
+      if(mysqli_num_rows($playlistsQuery) == 0){
+        echo "<span class='noResults'>You don't have any playlists yet.</span>";
+        };
+
+      while($row = mysqli_fetch_array($playlistsQuery)) {
+
+        $playlist = new Playlist($con, $row);
+
+        echo "<div class='gridViewItem'>
+
+            <div class='playlistImage'>
+                <img src='assets/images/icons/playlist.png'>
+            </div>
+        
+            <div class='gridViewInfo'>"
+            .$playlist->getName().
+            "</div>
+        </div>";
+      }
+     ?>
+
+
     </div>
 
 </div>
